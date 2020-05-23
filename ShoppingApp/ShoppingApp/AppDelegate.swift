f438f2132b59fb53c62581e348b41c2d87ef8939
jonathan garcia <jonathan.g@geopagos.com>
2020-05-23 14:55:37 -0300

    create project

---
+++ ShoppingApp/ShoppingApp/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  ShoppingApp
+//
+//  Created by Jonathan Garcia on 23/05/2020.
+//  Copyright © 2020 Jonathan Garcia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
+
---
+++ ShoppingApp/ShoppingApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Jonathan Garcia on 23/05/2020.
+//  Copyright © 2020 Jonathan Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
